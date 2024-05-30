a3b1b75b8cd9cf0e0543279bdb93788b7c9a487c
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-05-30 22:03:57 +0200

    Add tailwindcss with postcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.css";
+  import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<div class="merriweather-regular">
+  <Navbar />
+  <slot />
+</div>
+
+<style>
+  .merriweather-regular {
+    font-family: "Merriweather", serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style>
